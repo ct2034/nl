70979ff50d8d371aed9739f0432fc6f1021aaa70
ct2034 <post@henkelchristian.de>
2018-10-27 15:40:06 +0200

    little animation when turning on and off

--- nl.ino
+++ nl.ino
@@ -2,10 +2,13 @@
 #define NUM_LEDS 8
 #define DATA_PIN 7
 #define CLOCK_PIN 5
+#define DARK_THRESHHOLD 50
+#define ANIM_FRAME 100
+
 int pir = 0;
 int ldr = 0;
 int white_bright = 255;       // default brightness for white (0-255)
-int buzz_disable = 0;         // change to 1 to switch off initial buzzer
+int buzz_disable = 1;         // change to 1 to switch off initial buzzer
 
 CRGBArray<NUM_LEDS> leds;
 
@@ -23,6 +26,16 @@ void setup() {
   delay(1000);
   digitalWrite(2, LOW);       // buzzer OFF
   digitalWrite(13, LOW);      // PIR LED OFF
+
+  // DEMO
+  randomSeed(analogRead(0));
+  for(int t=0; t<NUM_LEDS; t++){
+    for(int i=0; i<t; i++){
+      leds[i].setRGB(random(0, 120), random(0, 120), random(0, 120));        
+     }
+     FastLED.show();
+     delay(ANIM_FRAME);   
+  }    
 }
 
 void loop() {
@@ -34,29 +47,41 @@ void loop() {
       ldr = analogRead(7);                     // read light sensor (0 = very dark, 1023 = very bright)
                                                //      50-150 is usually a suitable threshold
       if (pir == 1){                           // If motion detected
-        digitalWrite(13, HIGH);                // PIR LED ON
+        //digitalWrite(13, HIGH);                // PIR LED ON
         delay(100);                            // Delay so when dark the LED will flash on for 0.1s
       }
       else{
         digitalWrite(13, LOW);                 // PIR LED OFF
       }
-      //if (pir == 1 && ldr <= 150 ) {         // if someone there AND dark(ish) no case
-      if (pir == 1 && ldr <= 50 ) {            // if someone there AND dark(ish) with case
+      if (pir == 1 && ldr <= DARK_THRESHHOLD ) {          
         digitalWrite(13, LOW);                 // Don't need PIR LED on now
+        
+        // to on
+        for(int t=0; t<NUM_LEDS; t++){
+          for(int i=0; i<t; i++){
+            leds[i].setRGB(random(t*10, 120) / 2, random(t*10, 120), random(t*10, 120));        
+           }
+           FastLED.show();
+           delay(ANIM_FRAME);   
+        }    
+
+        // on
         for(int i=0; i<NUM_LEDS; i++){
-            leds[i].setRGB(white_bright, white_bright, white_bright);
+            leds[i].setRGB(white_bright/2, white_bright, white_bright);
         }
         FastLED.show();
         delay(18000);                          // lights on for about 18 seconds
 
-        for(int j=white_bright; j>-1; j--){ 
-           for(int i=0; i<NUM_LEDS; i++){
-              leds[i].setRGB(j, j, j);         // fading out the white over ~2s
+        // to off
+        for(int t=NUM_LEDS; t>0; t--){
+          for(int i=0; i<t; i++){
+            leds[i].setRGB(random(t*10, 120) / 2, random(t*10, 120), random(t*10, 120));        
            }
-          FastLED.show();
-          delay(10);                           // wait 10ms
-        }
+           FastLED.show();
+           delay(ANIM_FRAME);   
+        }    
 
+        // off
         FastLED.clear();                       // reset LEDs to 0,0,0
         FastLED.show();                        // lights off
       }
